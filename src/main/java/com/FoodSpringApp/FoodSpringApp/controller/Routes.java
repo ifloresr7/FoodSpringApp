46a392bacb88050af37883d994350ab0866a323b
cbravose <cbravose@uoc.edu>
2024-10-15 11:30:18 +0200

    actualizar msg  inicial

--- src/main/java/com/FoodSpringApp/FoodSpringApp/controller/Routes.java
+++ src/main/java/com/FoodSpringApp/FoodSpringApp/controller/Routes.java
@@ -1,10 +1,15 @@
 package com.FoodSpringApp.FoodSpringApp.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController; 
+
 
 @RestController
 public class Routes {
+   
+   /**
     @GetMapping("/")
     public String home() {
         return "Welcome to FoodSpringApp";
@@ -12,6 +17,29 @@ public class Routes {
 
     @GetMapping("/hola")
     public String holaMundo() {
+        
         return "¡Hola Mundo!";
+        
     }
+        */
+
+    @GetMapping("/saludo")
+	public String saludo(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "saludo";
+	}
 }
+    
+
+/**
+@Controller
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+}
+    */
